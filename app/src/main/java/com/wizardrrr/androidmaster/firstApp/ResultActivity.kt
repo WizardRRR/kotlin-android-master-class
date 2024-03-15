7dec6e0df650b11ef59f98bcbac87feb3a7bb349
Royer Rutti Ramos <ruttiramosroyer@gmail.com>
2024-03-14 19:25:21 -0500

    activitys-and-intents

--- app/src/main/java/com/wizardrrr/androidmaster/firstApp/ResultActivity.kt
+++ app/src/main/java/com/wizardrrr/androidmaster/firstApp/ResultActivity.kt
@@ -1,38 +1,26 @@
 package com.wizardrrr.androidmaster.firstApp
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
-import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.wizardrrr.androidmaster.R
 
-class FirstAppActivity : AppCompatActivity() {
+class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_first_app)
+        setContentView(R.layout.activity_result)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
-        val etName = findViewById<AppCompatEditText>(R.id.etName)
-
-
-        btnStart.setOnClickListener {
-            val name = etName.text.toString()
-            if (name.isNotEmpty()) {
-               val intent = Intent(this, ResultActivity::class.java)
-                intent.putExtra("EXTRA_NAME",name)
-                startActivity(intent)
-            }
-        }
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+        val name :String = intent.extras?.getString("EXTRA_NAME").orEmpty()
+        tvResult.text = "Hola $name"
     }
 }
